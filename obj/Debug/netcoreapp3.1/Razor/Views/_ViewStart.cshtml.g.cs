58d79a821891002cd9ed387d639858fb9bc0c4dd
Eduardo Hurtado <leduardohurtado@estudiante.uniajc.edu.co>
2020-11-03 17:26:54 -0500

    Version 1.1.2 - Fixed bootstrap 4.

--- obj/Debug/netcoreapp3.1/Razor/Views/_ViewStart.cshtml.g.cs
+++ obj/Debug/netcoreapp3.1/Razor/Views/_ViewStart.cshtml.g.cs
@@ -27,7 +27,7 @@ using net_core_web.ViewModels;
 #line hidden
 #nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"c0cb7f826af1de6ea245782a012ca2e0f93097b2", @"/Views/_ViewStart.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"39fd805b18be2e3da14ea6452ea3f7e266ff9019", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"907d0dd20376b915b150adb6f203a72197c5f0ae", @"/Views/_ViewImports.cshtml")]
     public class Views__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
